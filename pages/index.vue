9425eff1be8e1f4976d8ad3b3150c635eaf9e6b3
Douglas Anaya <102629422+Zaindou@users.noreply.github.com>
2023-02-09 00:07:34 -0500

    First project

---
+++ pages/index.vue
@@ -0,0 +1,88 @@
+<template>
+  <v-stepper v-model="e1">
+    <v-stepper-header>
+      <v-stepper-step :complete="e1 > 1" step="1">
+        Inputs datos
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step :complete="e1 > 2" step="2">
+        OTP
+      </v-stepper-step>
+
+      <v-divider></v-divider>
+
+      <v-stepper-step :complete="e1 > 3" step="3">
+        información financiera
+      </v-stepper-step>
+      <v-divider></v-divider>
+
+      <v-stepper-step :complete="e1 > 4" step="4">
+        ¿Felicidades?
+      </v-stepper-step>
+    </v-stepper-header>
+
+    <v-stepper-items>
+      <v-stepper-content step="1">
+        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+        <v-btn color="primary" @click="e1 = 2">
+          Continuar
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="2">
+        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+        <v-btn color="primary" @click="e1 = 3">
+          Continuar
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+      <v-stepper-content step="3">
+        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+        <v-btn color="primary" @click="e1 = 1">
+          Continuar
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+      <v-stepper-content step="4">
+        <v-card class="mb-12" color="grey lighten-1" height="200px"></v-card>
+
+        <v-btn color="primary" @click="e1 = 1">
+          Continuar
+        </v-btn>
+
+        <v-btn text>
+          Cancel
+        </v-btn>
+      </v-stepper-content>
+
+    </v-stepper-items>
+  </v-stepper>
+</template>
+<script>
+export default {
+  data(){
+    return {
+      head : {
+        title: 'Diagnostico Stepper TEST'
+      },
+      e1: 1
+    }
+  }
+}
+</script>
